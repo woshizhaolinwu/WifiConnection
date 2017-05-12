0d81cc085c7310fbb6bdf8642c56ddd7a5aa47bb
wuzhaolin <wuzhaolin@qq.com>
2017-05-12 18:11:01 +0800

    add code

---
+++ WifiConnectService/app/src/main/java/jrdcom/com/wificonnectservice/SplashActivity.java
@@ -0,0 +1,26 @@
+package jrdcom.com.wificonnectservice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by longcheng on 2017/5/12.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    private Button mButton;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView(){
+
+    }
+}
